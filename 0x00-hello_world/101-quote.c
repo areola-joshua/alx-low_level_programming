64e7614bf0cf09654d5ec9ca6d5522f2970b4c5c
areola-joshua <areolajoshua622@gmail.com>
2023-06-15 04:21:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A c program hat prints a line sandard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", sdout);
+	return (1);
+}
