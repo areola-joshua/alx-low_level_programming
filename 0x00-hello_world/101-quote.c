8af7261a325a34aecd03f1d33a974a69602d1965
areola-joshua <areolajoshua622@gmail.com>
2023-06-15 04:25:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", sdout);
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 	return (1);
 }
